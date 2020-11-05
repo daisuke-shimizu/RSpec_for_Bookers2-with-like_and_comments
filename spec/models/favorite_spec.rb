a36775d3a3d46d4a53d273ba2322418b8dcabba4
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2020-11-05 05:29:59 +0000

    made favorite.rb

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,1 +1,22 @@
 require 'rails_helper'
+RSpec.describe 'Favoriteモデルのテスト', type: :model do
+    describe 'バリデーションのテスト' do
+      let(:user) { create(:user) }
+      let(:book) { build(:book, user_id: user.id) }
+      # let!とすると、毎回データを作成してくれる
+      let!(:favorite) { build(:favorite, book_id: book.id, user_id: user.id) }
+    end
+
+    describe 'アソシエーションのテスト' do
+      context 'Userモデルとの関係' do
+        it 'N:1となっている' do
+          expect(Favorite.reflect_on_association(:user).macro).to eq :belongs_to
+        end
+      end
+      context 'Bookモデルとの関係' do
+          it 'N:1となっている' do
+            expect(Favorite.reflect_on_association(:book).macro).to eq :belongs_to
+          end
+        end
+    end
+  end
