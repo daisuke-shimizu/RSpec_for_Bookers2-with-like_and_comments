27d35bb46790e28f6c0f5b07bb9b419ce5b66606
infratop_mentor <GoodCrewMentor>
2020-11-09 05:06:59 +0000

    rspec for favorite_model done

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -2,11 +2,22 @@ require 'rails_helper'
 RSpec.describe 'Favoriteモデルのテスト', type: :model do
     describe 'バリデーションのテスト' do
       let(:user) { create(:user) }
-      let(:book) { build(:book, user_id: user.id) }
-      # let!とすると、毎回データを作成してくれる
-      let!(:favorite) { build(:favorite, book_id: book.id, user_id: user.id) }
-    end
+      let(:book) { create(:book, user_id: user.id) }
 
+      context 'user_idカラム' do
+
+        # let!とすると、毎回データを作成してくれる
+          let!(:favorite_first) { create(:favorite, book_id: book.id, user_id: user.id) }
+          it '1ユーザーが1つの本に対し、1回目のいいねができる' do
+            expect(favorite_first.valid?).to eq true;
+          end
+          # buildで1ユーザーが1つの本に対し、2回目のいいねするという情報のレコードを作成。それがsaveされるかどうか？
+        let!(:favorite_second) { build(:favorite, book_id: book.id, user_id: user.id) }
+          it '1ユーザーが1つの本に対し、2回以上いいねできない' do
+            expect(favorite_second.save).to eq false;
+          end
+      end
+    end
     describe 'アソシエーションのテスト' do
       context 'Userモデルとの関係' do
         it 'N:1となっている' do
