ee955d02f6d14024445644681bf54755dce81de7
infratop_mentor <GoodCrewMentor>
2020-11-10 01:31:16 +0000

    start controllers test

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,6 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
   #以下追加。
-  validates :user_id, presence: true, uniqueness: {scope: :book_id}
-  validates :book_id, presence: true
-  
+  validates :book_id, presence: true, uniqueness: {scope: :user_id}
+
 end
